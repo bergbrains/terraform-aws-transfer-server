1c6f5a3d933890d3333eb1e89f067c4fb6f040fa
Eric Berg <eric.berg@brace.ai>
2020-06-13 10:57:44 -0400

    Redid IAM permissions, based on docs

--- iam.tf
+++ iam.tf
@@ -10,28 +10,38 @@ data "aws_iam_policy_document" "transfer_server_assume_role" {
   }
 }
 
-data "aws_iam_policy_document" "transfer_server_assume_policy" {
+data "aws_iam_policy_document" "transfer_server_assume" {
+  statement {
+    sid = "AllowListingFolders"
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation"
+    ]
+    effect = "Allow"
+    resources = [
+      var.bucket_arn
+    ]
+  }
+
   statement {
     effect = "Allow"
 
     actions = [
-      "s3:DeleteObject",
-      "s3:DeleteObjectVersion",
-      "s3:GetBucketLocation",
-      "s3:GetObject",
-      "s3:GetObjectVersion",
-      "s3:ListBucket",
       "s3:PutObject",
+      "s3:GetObject",
+      "s3:List*",
+      "s3:DeleteObjectVersion",
+      "s3:DeleteObject",
+      "s3:GetObjectVersion"
     ]
 
     resources = [
-      var.bucket_arn,
       "${var.bucket_arn}/*"
     ]
   }
 }
 
-data "aws_iam_policy_document" "transfer_server_to_cloudwatch_assume_policy" {
+data "aws_iam_policy_document" "transfer_server_to_cloudwatch_assume" {
   statement {
     effect = "Allow"
 
@@ -45,19 +55,25 @@ data "aws_iam_policy_document" "transfer_server_to_cloudwatch_assume_policy" {
   }
 }
 
-resource "aws_iam_role" "transfer_server_role" {
-  name               = "${var.transfer_server_name}-transfer_server_role"
+resource "aws_iam_role" "transfer_server" {
+  name               = "${var.transfer_server_name}-transfer_server"
   assume_role_policy = data.aws_iam_policy_document.transfer_server_assume_role.json
+  tags = merge(
+    var.additional_tags,
+    map(
+      "Name", var.transfer_server_name
+    )
+  )
 }
 
-resource "aws_iam_role_policy" "transfer_server_policy" {
-  name   = "${var.transfer_server_name}-transfer_server_policy"
-  role   = aws_iam_role.transfer_server_role.name
-  policy = data.aws_iam_policy_document.transfer_server_assume_policy.json
+resource "aws_iam_role_policy" "transfer_server" {
+  name   = "${var.transfer_server_name}-transfer_server"
+  role   = aws_iam_role.transfer_server.name
+  policy = data.aws_iam_policy_document.transfer_server_assume.json
 }
 
-resource "aws_iam_role_policy" "transfer_server_to_cloudwatch_policy" {
-  name   = "${var.transfer_server_name}-transfer_server_to_cloudwatch_policy"
-  role   = aws_iam_role.transfer_server_role.name
-  policy = data.aws_iam_policy_document.transfer_server_to_cloudwatch_assume_policy.json
+resource "aws_iam_role_policy" "transfer_server_to_cloudwatch" {
+  name   = "${var.transfer_server_name}-transfer_server_to_cloudwatch"
+  role   = aws_iam_role.transfer_server.name
+  policy = data.aws_iam_policy_document.transfer_server_to_cloudwatch_assume.json
 }
